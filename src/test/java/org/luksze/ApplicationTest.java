a0d9fd7db2773f4fa3083e5607f20f36de33ec1a
Lukasz Szewc <lukasz-szewc@users.noreply.github.com>
2018-05-20 20:15:08 +0200

    simple test provided

---
+++ src/test/java/org/luksze/ApplicationTest.java
@@ -0,0 +1,19 @@
+package org.luksze;
+
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.DirtiesContext;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@DirtiesContext
+public class ApplicationTest {
+
+    @Test
+    public void name() {
+        Assert.assertTrue(true);
+    }
+}
